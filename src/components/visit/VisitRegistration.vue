4d2d792b87e9ab75b5bbf4553a7e89cb28513086
Marc Doerflinger <github@doerf.li>
2020-06-24 22:33:33 +0200

    submit registration (closes #1)

--- src/components/visit/VisitRegistration.vue
+++ src/components/visit/VisitRegistration.vue
@@ -3,13 +3,43 @@
     <h1 class="title">{{$t("visit.title.welcome", {name: this.$store.state.visit.location.name })}}</h1>
     <h2 class="subtitle">{{$t("visit.title.registration")}}</h2>
     <VisitorForm ref="form"/>
-    <button class="button is-primary" v-on:click="saveVisit()">{{$t("actions.register")}}</button>
+    <div class="field is-grouped">
+      <div class="control">
+        <button class="button is-primary" v-on:click="saveVisit()" v-bind:disabled="!formValid()">{{$t("actions.register")}}</button>
+      </div>
+    </div>
+    <div class="notification is-success" v-if="dataSubmitted">
+      <div class="columns">
+        <div class="column is-1">
+          <span class="icon is-large">
+            <i class="fas fa-3x fa-check-circle"></i>
+          </span>
+        </div>
+        <div class="column">
+          {{$t('visit.data_submitted')}}<br/>
+          {{$t('visit.data_submitted_retention_policy')}}
+        </div>
+      </div>
+    </div>
+    <div class="notification is-danger" v-if="errorDuringSubmission">
+      <div class="columns">
+        <div class="column is-1">
+          <span class="icon is-large">
+            <i class="fas fa-3x fa-exclamation-circle"></i>
+          </span>
+        </div>
+        <div class="column">
+          {{$t('visit.data_submission_error')}}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
   import {Component, Prop, Vue} from 'vue-property-decorator';
   import VisitorForm from '@/components/visit/VisitorForm.vue';
+  import {request} from '@/superagent';
 
   @Component({
     components: {
@@ -24,9 +54,42 @@
 
     @Prop()
     private shortname!: string;
+    private dataSubmitted: boolean = false;
+    private errorDuringSubmission: boolean = false;
 
     public mounted(): void {
       this.$store.dispatch("visit/getLocation", { shortname: this.shortname });
+    }
+
+    private formValid(): boolean {
+      // noinspection JSIncompatibleTypesComparison
+      if (undefined === this.$refs.form) {
+        return false;
+      }
+      return this.$refs.form.isValid() && ! this.dataSubmitted;
+    }
+
+    private async saveVisit() {
+      this.errorDuringSubmission = false;
+      const data = this.$refs.form.getFormData();
+      data.locationShortname = this.shortname;
+      try {
+        const response = await request.post(
+          "/api/visits"
+        ).send(data);
+        console.log(0);
+        console.log(response);
+        if (response.status === 200) {
+          this.dataSubmitted = true;
+        } else {
+          this.errorDuringSubmission = true;
+        }
+      } catch (e) {
+        console.log(1);
+        console.log(e);
+        this.errorDuringSubmission = true;
+      }
+
     }
 
   }
@@ -35,5 +98,8 @@
 <style scoped>
   button {
     margin-top: 20px;
+  }
+  .textWithIconLeft {
+    margin-left: 48px;
   }
 </style>
--- src/components/visit/VisitorForm.vue
+++ src/components/visit/VisitorForm.vue
@@ -2,7 +2,10 @@
     <div>
         <TextInputField :label="$t('visit.firstname')" v-model="firstname" v-on:inputchanged="$emit('formchanged')"/>
         <TextInputField :label="$t('visit.lastname')" v-model="lastname" v-on:inputchanged="$emit('formchanged')"/>
-        <TextInputField :label="$t('visit.email')" v-model="email" v-on:inputchanged="$emit('formchanged')"/>
+        <TextInputField :label="$t('visit.email')" v-model="email" v-on:inputchanged="checkEmailValid() && $emit('formchanged')"
+            :error-text="$t('visit.email_not_valid')"
+            v-bind:show-error-text="!emailValid"
+        />
         <TextInputField :label="$t('visit.phone')" v-model="phone" v-on:inputchanged="$emit('formchanged')"/>
     </div>
 </template>
@@ -10,6 +13,8 @@
 <script lang="ts">
   import {Component, Vue} from 'vue-property-decorator';
   import TextInputField from '@/components/form/TextInputField.vue';
+
+  const EMAIL_REGEX = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
   @Component({
     components: {
@@ -21,6 +26,8 @@
     private lastname: string = "";
     private phone: string = "";
     private email: string = "";
+    private emailValid: boolean = true;
+
 
     public getFormData() {
       return {
@@ -30,6 +37,16 @@
         email: this.email
       };
     }
+
+    public isValid(): boolean {
+        return !(this.email.trim() === "" || !this.emailValid);
+
+    }
+
+    private checkEmailValid() {
+        this.emailValid = EMAIL_REGEX.test(this.email);
+    }
+
   }
 </script>
 
