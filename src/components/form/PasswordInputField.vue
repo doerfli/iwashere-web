593d80eaee5c2d7de79e3ca83b3d09e0e6e8e5f8
Marc Doerflinger <github@doerf.li>
2020-06-25 21:20:00 +0200

    simplify login form (#9)

---
+++ src/components/form/PasswordInputField.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="field">
+        <label class="label">{{this.getLabel()}}</label>
+        <div v-bind:class="{ control: true, 'has-icons-left': hasIconLeft() }">
+            <input class="input" type="password"
+                   v-bind:value="value"
+                   :placeholder="getHintText()"
+                   v-on:input="$emit('input', $event.target.value)"
+                   v-on:change="$emit('inputchanged', value)"
+            />
+            <span class="icon is-small is-left" v-if="hasIconLeft()">
+              <i :class="getIconLeftClasses()"></i>
+            </span>
+        </div>
+        <p class="help is-danger" v-if="isShowErrorText()">{{getErrorText()}}</p>
+        <p class="help is-info" v-if="hasInfoText()">{{getInfoText()}}</p>
+    </div>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from 'vue-property-decorator';
+
+  @Component
+  export default class TextInputField extends Vue {
+    @Prop()
+    private label!: string;
+    @Prop({default: ""})
+    private value!: string;
+    @Prop()
+    private errorText!: string;
+    @Prop({default: false})
+    private showErrorText!: boolean;
+    @Prop()
+    private infoText!: string;
+    @Prop()
+    private hintText!: string;
+    @Prop()
+    private iconLeft!: string;
+
+    private getLabel() {
+      return this.label;
+    }
+
+    private getErrorText() {
+      return this.errorText;
+    }
+
+    private isShowErrorText() {
+
+      return this.showErrorText == null || this.showErrorText;
+    }
+
+    private getInfoText() {
+      return this.infoText;
+    }
+
+    private hasInfoText(): boolean {
+      return this.infoText != null && this.infoText.trim() !== "";
+    }
+
+    private getHintText() {
+        return this.hintText;
+    }
+
+    private hasIconLeft() {
+        return this.iconLeft != null;
+    }
+
+    private getIconLeft() {
+        return this.iconLeft;
+    }
+
+      private getIconLeftClasses() {
+          return `fas fa-${this.iconLeft}`;
+      }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/login/LoginForm.vue
+++ src/components/login/LoginForm.vue
@@ -2,32 +2,23 @@
   <div class="loginform">
     <TextInputField :label="$t('login.username.label')" :hint-text="$t('login.username.hint')"
                     icon-left="user"
-                    v-model="username" />
-    <div class="field">
-      <label class="label">{{$t('login.password.label')}}</label>
-      <div class="control has-icons-left has-icons-right">
-        <input class="input" type="text" :placeholder="$t('login.password.hint')"
-               v-model="password">
-        <span class="icon is-small is-left">
-                              <i class="fas fa-key"></i>
-                            </span>
-      </div>
-    </div>
-    <div class="field">
-      <p class="control">
-        <button class="button is-primary" v-on:click="login">
-          {{ $t("login.loginbutton")}}
-        </button>
-      </p>
-    </div>
+                    v-model="username"
+    />
+    <PasswordInputField :label="$t('login.password.label')" :hint-text="$t('login.password.hint')"
+                        icon-left="key"
+                        v-model="password"
+    />
+    <Button :title="$t('login.loginbutton')" v-on:click="login" />
   </div>
 </template>
 
 <script lang="ts">
   import {Component, Vue} from 'vue-property-decorator';
   import TextInputField from '@/components/form/TextInputField.vue';
+  import PasswordInputField from '@/components/form/PasswordInputField.vue';
+  import Button from '@/components/form/Button.vue';
   @Component({
-    components: {TextInputField}
+    components: {Button, PasswordInputField, TextInputField}
   })
   export default class LoginForm extends Vue {
     private username: string = "";
