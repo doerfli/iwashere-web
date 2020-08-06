f5e98a4a79c4733aeca378602b058eb5fc26645f
Marc Doerflinger <marc@doerf.li>
2020-08-06 23:38:56 +0200

    cleanup

--- _vue_classic/src/i18n/locales.ts
+++
@@ -1,9 +0,0 @@
-export enum Locales {
-  DE = 'de',
-  EN = 'en',
-}
-
-export const LOCALES = [
-  { value: Locales.DE, caption: 'Deutsch' },
-  { value: Locales.EN, caption: 'English' },
-];
--- _vue_classic/src/model/visitentity.ts
+++
@@ -1,9 +0,0 @@
-interface VisitEntity {
-  id: number;
-  guest_name: string;
-  guest_email: string | null;
-  guest_phone: string | null;
-  visitTimestamp: string;
-}
-
-export default VisitEntity;
