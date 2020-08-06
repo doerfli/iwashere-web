8a0c2d3d3cd6c7f61a09c71689fc526ebc04f5ab
Marc Doerflinger <marc@doerf.li>
2020-08-07 00:16:26 +0200

    add index page and IchWarDaTitle component

---
+++ components/IchWarDaTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <span><span class="has-text-primary">Ich war da</span></span>
+</template>
+
+<script lang="ts">
+  import {Vue} from "vue-property-decorator";
+
+  export default class IchWarDaTitle extends Vue {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
+
