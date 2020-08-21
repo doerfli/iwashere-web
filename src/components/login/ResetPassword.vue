db64f43f2048ba1ce40d7275f1e34e61665d9102
Marc Doerflinger <marc@doerf.li>
2020-08-21 22:38:58 +0200

    handle token expiration (closes doerfli/iwashere#25)

--- src/components/login/ResetPassword.vue
+++ src/components/login/ResetPassword.vue
@@ -5,8 +5,20 @@
       <Button :title="$t('reset_password.submit')" v-on:click="resetPassword" />
     </div>
 
-    <div class="notification is-danger" v-if="showPasswordResetError">
+    <div class="notification is-success" v-if="showResetSuccess">
+      {{$t('reset_password.reset_success')}}
+    </div>
+    <div class="notification is-warning" v-if="showInvalidToken">
+      {{$t('reset_password.invalid_token')}}
+    </div>
+    <div class="notification is-warning" v-if="showTokenExpired">
+      {{$t('reset_password.token_expired')}}
+    </div>
+    <div class="notification is-danger" v-if="showError">
       {{$t('reset_password.error')}}
+    </div>
+    <div>
+      <router-link to="/home">{{$t('actions.back')}}</router-link>
     </div>
   </div>
 </template>
@@ -15,8 +27,7 @@
 import {Component, Prop, Vue} from 'vue-property-decorator';
 import PasswordFormFields from '@/components/login/PasswordFormFields.vue';
 import Button from '@/components/form/Button.vue';
-import {request} from '@/superagent';
-import router from '@/router';
+import superagent from 'superagent';
 
 @Component({
     components: {Button, PasswordFormFields}
@@ -28,23 +39,39 @@ import router from '@/router';
 
     @Prop()
     private token!: string;
-    private showPasswordResetError: boolean = false;
+    private showResetSuccess: boolean = false;
+    private showInvalidToken: boolean = false;
+    private showTokenExpired: boolean = false;
+    private showError: boolean = false;
 
     private async resetPassword() {
       if ( ! this.$refs.passwordFields.isValid()) {
         return;
       }
 
+      this.showResetSuccess = false;
+      this.showInvalidToken = false;
+      this.showTokenExpired = false;
+      this.showError = false;
+
       try {
-        await request.post('/api/accounts/resetPassword').send({
+        await superagent.post('/api/accounts/resetPassword').send({
           token: this.token,
           password: this.$refs.passwordFields.getPassword()
         });
         console.log("password reset successful");
-        await router.push({name: 'Home', query: { resetPassword: 'true' }});
+        this.showResetSuccess = true;
       } catch (e) {
         console.log(e.response);
-        this.showPasswordResetError = true;
+        if (e.response.status === 400) {
+          this.showInvalidToken = true;
+        } else if (e.response.status === 409) {
+          this.showInvalidToken = true;
+        } else if (e.response.status === 401) {
+          this.showTokenExpired = true;
+        } else {
+          this.showError = true;
+        }
       }
     }
   }
